8c6ddf89abcbbf361669f69f34d26a5a82d6e3db
PayalNandre <123804088+PayalNandre@users.noreply.github.com>
2023-06-22 18:19:16 +0530

    Add files via upload

--- src/Components/NewTransaction.jsx
+++ src/Components/NewTransaction.jsx
@@ -9,31 +9,19 @@ const Container = styled(Box)`
     }
 `;
 
-const StyledButton = styled(Button)`
-    background: #445A6F;
-    color: #fff;
-`;
-
-const NewTransaction = ({ addTransaction }) => {
+const NewTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState();
 
-    const newTransaction = e => {
-        const transaction = {
-            id: Math.floor(Math.random() * 100000000),
-            text: text,
-            amount: +amount
-        }
-        addTransaction(transaction);
-    }
 
     return (
         <Container>
             <Typography variant="h5">New Transaction</Typography>
             <TextField value={text} label="Enter Expense" onChange={(e) => setText(e.target.value)} />
             <TextField value={amount} label="Enter Amount" onChange={(e) => setAmount(e.target.value)} />
-            <StyledButton variant="contained" onClick={newTransaction}>Add Transaction</StyledButton>
+            <Button variant="contained" onClick={NewTransaction}>Add Transaction</Button>
         </Container>
     )
+
 }
 export default NewTransaction;
